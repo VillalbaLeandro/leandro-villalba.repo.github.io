9b344b16d00005747760388e1721d8b4e6abec81
VillalbaLeandro <exemprary@gmail.com>
2023-07-08 02:14:40 -0300

    se agrego el menu navbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,128 +1,121 @@
 window.onload = (event) => {
 
-  const menuHamburguesa = document.querySelector(".menu-hamburguesa-container");
-  const menuContainer = document.querySelector(".menu-container");
-  const menuNavbar = document.querySelector(".menu-navbar");
+  // const menuHamburguesa = document.querySelector(".menu-hamburguesa-container");
+  // const menuContainer = document.querySelector(".menu-container");
+  // const menuNavbar = document.querySelector(".menu-navbar");
 
 
-  // opacity: 1;
-  // background-color: black;
-  // left: 0;
-  menuHamburguesa.addEventListener(`click` ,()=>{
-      menuNavbar.style.opacity = 1;
-      menuNavbar.style.backgroundColor = "rgb(4, 2, 22)";
-      menuNavbar.style.left = 0;
-      menuNavbar.style.boxShadow = "18px 16px 20px -5px rgba(0,0,0,0.75)";
+  // // opacity: 1;
+  // // background-color: black;
+  // // left: 0;
+  // menuHamburguesa.addEventListener(`click` ,()=>{
+  //     menuNavbar.style.opacity = 1;
+  //     menuNavbar.style.backgroundColor = "rgb(4, 2, 22)";
+  //     menuNavbar.style.left = 0;
+  //     menuNavbar.style.boxShadow = "18px 16px 20px -5px rgba(0,0,0,0.75)";
       
-  })
-  menuNavbar.addEventListener(`mouseleave` ,()=>{
-      menuNavbar.style.opacity = 0;
-      menuNavbar.style.backgroundColor = "null";
-      menuNavbar.style.left = "-100%";
+  // })
+  // menuNavbar.addEventListener(`mouseleave` ,()=>{
+  //     menuNavbar.style.opacity = 0;
+  //     menuNavbar.style.backgroundColor = "null";
+  //     menuNavbar.style.left = "-100%";
       
-  })
+  // })
 
 
+// carrusel
 
-  // 
-  
 /*--------------------
 Vars
 --------------------*/
-let progress = 50
-let startX = 0
-let active = 0
-let isDown = false
+let progress = 50;
+let startX = 0;
+let active = 0;
+let isDown = false;
 
 /*--------------------
 Contants
 --------------------*/
-const speedWheel = 0.02
-const speedDrag = -0.1
+const speedWheel = 0.02;
+const speedDrag = -0.1;
 
 /*--------------------
 Get Z
 --------------------*/
-const getZindex = (array, index) => (array.map((_, i) => (index === i) ? array.length : array.length - Math.abs(index - i)))
+const getZindex = (array, index) => (array.map((_, i) => (index === i) ? array.length : array.length - Math.abs(index - i)));
 
 /*--------------------
 Items
 --------------------*/
-const $items = document.querySelectorAll('.carousel-item')
-const $cursors = document.querySelectorAll('.cursor')
+const $items = document.querySelectorAll('.carousel-item');
+const $cursors = document.querySelectorAll('.cursor');
 
 const displayItems = (item, index, active) => {
-  const zIndex = getZindex([...$items], active)[index]
-  item.style.setProperty('--zIndex', zIndex-2)
-  item.style.setProperty('--active', (index-active)/$items.length)
-}
+  const zIndex = getZindex([...$items], active)[index];
+  item.style.setProperty('--zIndex', zIndex);
+  item.style.setProperty('--active', (index - active) / $items.length);
+};
 
 /*--------------------
 Animate
 --------------------*/
 const animate = () => {
-  progress = Math.max(0, Math.min(progress, 100))
-  active = Math.floor(progress/100*($items.length-1))
-  
-  $items.forEach((item, index) => displayItems(item, index, active))
-}
-animate()
+  progress = Math.max(0, Math.min(progress, 100));
+  active = Math.floor(progress / 100 * ($items.length - 1));
+
+  $items.forEach((item, index) => displayItems(item, index, active));
+};
+animate();
 
 /*--------------------
 Click on Items
 --------------------*/
 $items.forEach((item, i) => {
   item.addEventListener('click', () => {
-    progress = (i/$items.length) * 100 + 10
-    animate()
-  })
-})
+    progress = (i / $items.length) * 100 + 10;
+    animate();
+  });
+});
 
 /*--------------------
 Handlers
 --------------------*/
-const handleWheel = e => {
-  const wheelProgress = e.deltaY * speedWheel
-  progress = progress + wheelProgress
-  animate()
-}
-
 const handleMouseMove = (e) => {
-  if (e.type === 'mousemove') {
-    $cursors.forEach(($cursor) => {
-      $cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`
-    })
-  }
-  if (!isDown) return
-  const x = e.clientX || (e.touches && e.touches[0].clientX) || 0
-  const mouseProgress = (x - startX) * speedDrag
-  progress = progress + mouseProgress
-  startX = x
-  animate()
-}
+  $cursors.forEach(($cursor) => {
+    $cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+  });
+  if (!isDown) return;
+
+  const x = e.clientX || (e.touches && e.touches[0].clientX) || 0;
+  const mouseProgress = (x - startX) * speedDrag;
+  progress = progress + mouseProgress;
+  startX = x;
+  animate();
+};
 
 const handleMouseDown = e => {
-  isDown = true
-  startX = e.clientX || (e.touches && e.touches[0].clientX) || 0
-}
+  isDown = true;
+  startX = e.clientX || (e.touches && e.touches[0].clientX) || 0;
+  document.addEventListener('mousemove', handleMouseMove);
+};
 
 const handleMouseUp = () => {
-  isDown = false
-}
+  if (isDown) {
+    isDown = false;
+    startX = 0;
+    document.removeEventListener('mousemove', handleMouseMove);
+  }
+};
 
 /*--------------------
 Listeners
 --------------------*/
-// document.addEventListener('mousewheel', handleWheel) //funcion para desplazarce con el scroll
-document.addEventListener('mousedown', handleMouseDown)
-document.addEventListener('mousemove', handleMouseMove)
-document.addEventListener('mouseup', handleMouseUp)
-document.addEventListener('touchstart', handleMouseDown)
-document.addEventListener('touchmove', handleMouseMove)
-document.addEventListener('touchend', handleMouseUp)
-
-
-
-
+// document.addEventListener('mousewheel', handleWheel);
+document.addEventListener('mousedown', handleMouseDown);
+document.addEventListener('mousemove', handleMouseMove);
+document.addEventListener('mouseup', handleMouseUp);
+document.addEventListener('touchstart', handleMouseDown);
+document.addEventListener('touchmove', handleMouseMove);
+document.addEventListener('touchend', handleMouseUp);
 
 };
