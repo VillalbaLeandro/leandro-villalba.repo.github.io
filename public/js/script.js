8038a0b05c77896b6932f9e0b61f0532cab2707c
VillalbaLeandro <exemprary@gmail.com>
2023-07-09 01:35:15 -0300

    probando opciones para ocultar la barra de navegacion

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,9 @@
 
-function hideAddressBar(){
-  if(document.documentElement.scrollHeight<window.outerHeight/window.devicePixelRatio)
-    document.documentElement.style.height=(window.outerHeight/window.devicePixelRatio)+'px';
-  setTimeout(window.scrollTo(1,1),0);
-}
-window.addEventListener("load",function(){hideAddressBar();});
-window.addEventListener("orientationchange",function(){hideAddressBar();});
+window.addEventListener("load",function() {
+  setTimeout(function(){
+  window.scrollTo(0, 1);
+  }, 0);
+  });
 
 window.onload = (event) => {
 
